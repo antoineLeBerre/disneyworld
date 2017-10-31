0ce9f27cfb3c5d44b750f19c0f47f65d6ae516c8
Antoine Le Berre <antoineleberre@MacBook-Pro-de-Antoine.local>
2017-10-31 15:22:52 +0100

    test Merge

--- src/Disneyland/disneyBundle/Controller/HomeController.php
+++ src/Disneyland/disneyBundle/Controller/HomeController.php
@@ -8,6 +8,9 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DisneylanddisneyBundle:Home:index.html.twig');
+        $imgsJson = file_get_contents('http://cdn1.dlp-media.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-dlp-assets/prod/fr-fr/system/images/n009365_2017jun01_disney-studio1_16-9.jpg');
+        return $this->render('DisneylanddisneyBundle:Home:index.html.twig', [
+            'image' => 'http://cdn1.dlp-media.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-dlp-assets/prod/fr-fr/system/images/n009365_2017jun01_disney-studio1_16-9.jpg'
+        ]);
     }
 }
